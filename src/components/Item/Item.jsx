f4656a1dd079b80f767f24edb3d5e14ff3107d85
Dima Morsch <noemail>
2024-02-05 14:24:21 +0200

    Export dateTimeConvert function to utils folder

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,20 +1,12 @@
 import {useState} from "react";
 import {MdCheck} from "react-icons/md";
 
-import API from "../../API.js";
+import dateTimeConvert from "../../utils/dateTimeConvert.js";
 
 import "./Item.css";
 
 function Item({item}) {
     const [isRead, setIsRead] = useState(item.markAsRead);
-
-    const dateTimeConvert = () => {
-        const itemDate = new Date(item.pubDate);
-        const isoDate = itemDate.toISOString();
-        const date = isoDate.substring(0, 10);
-        const time = itemDate.getHours() + ":" + isoDate.substring(14, 16);
-        return date + '\n' + time;
-    };
 
     const handleRead = () => {
         API.markItemAsRead(!isRead, item.id).then(() => {
@@ -23,11 +15,12 @@ function Item({item}) {
         });
     };
 
+function Item({item, onMarkRead}) {
     return (
         <>
             <div className="item-header">
                 <a href={item.link} className="title">{item.title}</a>
-                <span className="pubdate">{dateTimeConvert()}</span>
+                <span className="pubdate">{dateTimeConvert(item.pubDate)}</span>
             </div>
             {item.description && <div className="description" dangerouslySetInnerHTML={{__html: item.description}}/>}
 
---
+++ src/utils/dateTimeConvert.js
@@ -0,0 +1,7 @@
+export default function dateTimeConvert(value) {
+    const itemDate = new Date(value);
+    const isoDate = itemDate.toISOString();
+    const date = isoDate.substring(0, 10);
+    const time = itemDate.getHours() + ":" + isoDate.substring(14, 16);
+    return date + '\n' + time;
+}
